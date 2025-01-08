477b6fdf2c19b94857bc9e8e5db0bee3cc885099
Ankit <kashyap25ankit@gmail.com>
2025-01-09 00:59:12 +0530

    added prisma with postgres as db

--- app/routes/signin.tsx
+++ app/routes/signin.tsx
@@ -1,56 +1,41 @@
-import type { MetaFunction } from "@remix-run/node";
-import { Form, useActionData } from "@remix-run/react";
 import { ActionFunctionArgs, redirect } from "@remix-run/node";
+import { Form, useActionData } from "@remix-run/react";
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
-export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
-};
-
 export async function action({ request }: ActionFunctionArgs) {
   try {
-    const formData: FormData = await request.formData();
+    const formData = await request.formData();
     const name = formData.get("name") as string;
     const password = formData.get("password") as string;
-    if (name === "" || password === "") throw new Error("Validation error");
 
-    const isUserAlreadyThere = await prisma.user.findFirst({
+    if (name === "" || password === "") throw new Error("Vlaidation error");
+
+    const getUser = await prisma.user.findFirst({
       where: {
-        name: name,
-      },
-    });
-
-    if (isUserAlreadyThere) throw new Error("user already there");
-
-    await prisma.user.create({
-      data: {
         name: name,
         password: password,
       },
     });
 
-    return redirect("/signin");
+    if (!getUser) throw new Error("No user with this name exist");
+
+    return redirect("/dashboard");
   } catch (error) {
-    return Response.json(
-      { error: "true", message: (error as Error).message },
-      {
-        status: 400,
-      }
-    );
+    return Response.json({
+      status: 400,
+      message: (error as Error).message,
+      error: true,
+    });
   }
 }
 
-export default function Index() {
+export default function Signin() {
   const actionData = useActionData<typeof action>();
-
   return (
     <div className="flex justify-center items-center h-screen">
       <Form className="flex flex-col gap-4" method="post">
-        <p>Signup Form</p>
+        <p>Signin Form</p>
         <input
           className="border-2 border-neutral-700 outline-0 p-2 rounded-md"
           placeholder="name"
