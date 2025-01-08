e96f5ba46ff695fc743a94eeb3c648a334cb6465
Ankit <kashyap25ankit@gmail.com>
2025-01-08 23:50:22 +0530

    testing signup forms

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,6 @@
 import type { MetaFunction } from "@remix-run/node";
+import { Form, useActionData } from "@remix-run/react";
+import type { ActionFunctionArgs } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
   return [
@@ -7,6 +9,40 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export async function action({ request }: ActionFunctionArgs) {
+  try {
+    const formData: FormData = await request.formData();
+    console.log(formData);
+    if (formData.name === " " || formData.password === " ")
+      throw new Error("Validation error");
+
+    return Response.json({ status: "ok" });
+  } catch (error) {
+    return Response.json({ ok: "false" });
+  }
+}
+
 export default function Index() {
-  return <div>Hi</div>;
+  const actionData = useActionData<typeof action>();
+  console.log("actions data", actionData);
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <Form className="flex flex-col gap-4" method="post">
+        <p>Signup Form</p>
+        <input
+          className="border-2 border-neutral-700 outline-0 p-2 rounded-md"
+          placeholder="name"
+          name="name"
+        />
+        <input
+          className="border-2 border-neutral-700 outline-0 p-2 rounded-md"
+          placeholder="password"
+          name="password"
+        />
+        <button type="submit" className="p-4 rounded-lg bg-violet-400">
+          Submit
+        </button>
+      </Form>
+    </div>
+  );
 }
